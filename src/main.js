9e558e63432cc83ea490feafa4755c9e2a4e23a2
乔 <oncwnuHLBjIE5tvkCCqOQtY_KPU8@git.weixin.qq.com>
2020-05-19 15:41:38 +0800

    修改文件

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 强强测试
 import Vue from 'vue'
 import App from './App'
 
@@ -7,7 +8,7 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    components: { App },
+    template: '<App/>'
 })
